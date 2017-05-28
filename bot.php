87ef62a3cd8159a87b871797eb606bfe4f68a749
Boccolin <onthefunz@gmail.com>
2017-05-28 14:48:47 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,3 +1,5 @@
 <?php
-echo "bot !!!";
+curl -X GET \
+-H eL/jOW+9yOGBFlUwF5D63r4LfxzZprvRXxoQrRS+qRPbKQZ4N/orAh6mzU+ONYdbZJ6vY4H2swdTWy6b0QvjkW5THHPjHQoG3lVoQJeOs8v4fafTGQu8UlOkEDeNIAVAcgETQoXjNcPnZAhp+SIEfAdB04t89/1O/w1cDnyilFU= \
+https://api.line.me/v1/oauth/verify
 ?>
